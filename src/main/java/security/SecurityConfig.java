841dc24cf303df47c213d418b076334596ec18ca
oudom <oudomphoem@gmail.com>
2026-01-19 12:10:02 +0700

    update gateway server

---
+++ src/main/java/security/SecurityConfig.java
@@ -0,0 +1,31 @@
+package security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain webSecurity(ServerHttpSecurity http) {
+
+        http.authorizeExchange(exchange -> exchange
+                .anyExchange().permitAll()
+        );
+
+        http.csrf(csrfSpec -> csrfSpec.disable());
+        http.formLogin(formLoginSpec -> formLoginSpec.disable());
+        http.logout(logoutSpec -> logoutSpec.disable());
+        http.httpBasic(httpBasicSpec -> httpBasicSpec.disable());
+
+        http.oauth2Login(Customizer.withDefaults());
+
+
+        return http.build();
+    }
+}
