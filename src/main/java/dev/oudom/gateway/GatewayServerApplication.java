978b596f109dc3fccadde1b00d652aa956b73ee6
oudom <oudomphoem@gmail.com>
2026-02-02 23:41:33 +0700

    feat: add rate limiter pattern

--- src/main/java/dev/oudom/gateway/GatewayServerApplication.java
+++ src/main/java/dev/oudom/gateway/GatewayServerApplication.java
@@ -2,12 +2,20 @@ package dev.oudom.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class GatewayServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServerApplication.class, args);
+	}
+
+	@Bean
+	KeyResolver userKeyResolver() {
+		return exchange -> Mono.justOrEmpty(exchange.getRequest().getHeaders().getFirst("user")).defaultIfEmpty("anonymous");
 	}
 
 }
